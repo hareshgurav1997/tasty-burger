51d57a3f63921e6095559efd957809fc9a17a71c
Haresh Gurav <hareshgurav1997@gmail.com>
2025-05-15 04:03:09 +0530

    final changes

--- src/components/layouts/Cards.jsx
+++ src/components/layouts/Cards.jsx
@@ -1,7 +1,7 @@
+import PropTypes from "prop-types";
 import { Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-// eslint-disable-next-line react/prop-types
 export default function Cards({
   image,
   rating,
@@ -11,36 +11,49 @@ export default function Cards({
   renderRatingIcons,
 }) {
   return (
-    <>
-      <Col sm={6} lg={4} xl={3} className="mb-4">
-        <Card className="overflow-hidden">
-          <div className="overflow-hidden">
-            <Card.Img variant="top" src={image} />
+    <Col sm={6} lg={4} xl={3} className="mb-4">
+      <Card className="overflow-hidden">
+        <div className="overflow-hidden">
+          <Card.Img variant="top" src={image} />
+        </div>
+        <Card.Body>
+          <div className="d-flex align-items-center justify-content-between">
+            <div className="item_rating">{renderRatingIcons(rating)}</div>
+            <div className="wishlist">
+              <i className="bi bi-heart"></i>
+            </div>
           </div>
-          <Card.Body>
-            <div className="d-flex align-items-center justify-content-between">
-              <div className="item_rating">{renderRatingIcons(rating)} </div>
 
-              <div className="wishlist">
-                <i className="bi bi-heart"></i>
-              </div>
-            </div>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{paragraph}</Card.Text>
 
-            <Card.Title>{title}</Card.Title>
-            <Card.Text>{paragraph}</Card.Text>
-            <div className="d-flex align-items-center justify-content-between">
-              <div className="menu_price">
-                <h5 className="mb-0">${price}</h5>
-              </div>
-              <div className="add_to_card">
-                <Link to="/">
-                  <i className="bi bi-bag me-2"> </i> Add to Cart
-                </Link>
-              </div>
+          <div className="d-flex align-items-center justify-content-between">
+            <div className="menu_price">
+              <h5 className="mb-0">${price}</h5>
             </div>
-          </Card.Body>
-        </Card>
-      </Col>
-    </>
+            <div className="add_to_cart">
+              <Link to="/">
+                <i className="bi bi-bag me-2" /> Add to Cart
+              </Link>
+            </div>
+          </div>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 }
+
+Cards.propTypes = {
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+  title: PropTypes.string.isRequired,
+  paragraph: PropTypes.string,
+  price: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  renderRatingIcons: PropTypes.func,
+};
+
+Cards.defaultProps = {
+  rating: 0,
+  paragraph: "",
+  renderRatingIcons: () => null,
+};
