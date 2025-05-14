cd58d1f42e9400f90dfded3fd72db3e50abdbfcd
Haresh Gurav <hareshgurav1997@gmail.com>
2025-05-15 04:12:33 +0530

    final chan ge

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/tasty-burger/", // <-- adjust to your repo name
-  // build: {
-  //   outDir: "dist",
-  // },
+  build: {
+    outDir: "dist",
+  },
 });
